bf236018da767f08cbcdb8d50bbfca5676661655
Zahid <zahid1004048@gmail.com>
2025-07-16 22:16:05 +0600

    env

--- src/environments/environment-example.ts
+++ src/environments/environment-example.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: false,
-  // api_url: 'https://afcpharmacycumillaapi.cerebrum.host/',
   api_url: 'https://analyticalj.com/PharmacyLumen/',
-  // api_url: 'http://192.168.0.130/spe_api/',
   protocal: 'http://',
   storeHoshName: 'localhost:4200',
   patnerHoshName: 'localhost:4200',
